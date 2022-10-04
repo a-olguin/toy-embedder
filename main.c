efcfb8637eba26cfe6e467f80578925e3f45a686
Andrew O <andrew.t.olguin@gmail.com>
2022-10-04 07:11:59 +0000

    Added teeny embedding of interpreter. Next up: running a script

--- main.c
+++ main.c
@@ -3,8 +3,17 @@
 
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[]){
 
-    printf("Hello world!\n");
-    return 1;
+    printf("Hello world! I'm a C program!\n");
+
+    // Reference Embedding Python in Another Application. Locally, the docs were at file:///C:/Dev/Python-Docs/python-3.8.14-docs-html/extending/embedding.html
+
+    // Very High Level Embedding
+    Py_Initialize();
+    PyRun_SimpleString("print(\"Hello world! I'm a python string!\")");
+    if (Py_FinalizeEx() < 0){
+        return 120;
+    }
+    return 0;
 }
